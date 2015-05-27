8b774b419c0cfa6f309f396f075e3e7c44800ad3
Michael Tautschnig <mt@debian.org>
2015-05-27 11:42:42 +0100

    Added KLEE parser

---
+++ scripts/parse-klee.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl -w
+#
+# Copyright (c) 2011 Michael Tautschnig <michael.tautschnig@comlab.ox.ac.uk>
+#               2015 Michael Tautschnig <michael.tautschnig@qmul.ac.uk>
+# 
+# All rights reserved. Redistribution and use in source and binary forms, with
+# or without modification, are permitted provided that the following
+# conditions are met:
+# 
+#   1. Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+# 
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+# 
+#   3. All advertising materials mentioning features or use of this software
+#      must display the following acknowledgement:
+# 
+#      This product includes software developed by Michael Tautschnig,
+#      Queen Mary University of London
+# 
+#   4. Neither the name of the University nor the names of its contributors
+#      may be used to endorse or promote products derived from this software
+#      without specific prior written permission.
+# 
+#    
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS `AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+# parse the CPROVER tool specific part of a benchmark log file
+
+use strict;
+use warnings FATAL => qw(uninitialized);
+
+sub parse_log {
+  my ($LOG, $hash) = @_;
+  
+  $hash->{Result} = "SUCCESSFUL";
+
+  while (<$LOG>) {
+    chomp;
+    return 1 if (/^###############################################################################$/);
+
+    if (/^Timeout: aborting command/) {
+      $hash->{Result} = "TIMEOUT";
+    } elsif (/^KLEE: ERROR: :\d+: ASSERTION FAIL:.*$/) {
+      $hash->{Result} = "FAILED";
+    } elsif (/^KLEE: ERROR: .*$/) {
+      $hash->{Result} = "ERROR";
+    } elsif (/^KLEE: done: total instructions = (\d+)$/) {
+      $hash->{"instructions"} = $1;
+    } elsif (/^KLEE: done: completed paths = (\d+)$/) {
+      $hash->{"paths"} = $1;
+    } elsif (/^KLEE: done: generated tests = (\d+)$/) {
+      $hash->{"tests"} = $1;
+    }
+  }
+}
+
+return 1;
+
